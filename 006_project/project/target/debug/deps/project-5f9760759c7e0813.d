f0785dab61743d764e73abcb9c7d935efe278493
MatheusMendesL <matheusmendelopes@gmail.com>
2026-08-25 10:36:52 -0300

    project course

---
+++ 006_project/project/target/debug/deps/project-5f9760759c7e0813.d
@@ -0,0 +1,5 @@
+C:\Users\Matheus\OneDrive\Desktop\Programação\rust\006_project\project\target\debug\deps\project-5f9760759c7e0813.d: src\main.rs
+
+C:\Users\Matheus\OneDrive\Desktop\Programação\rust\006_project\project\target\debug\deps\libproject-5f9760759c7e0813.rmeta: src\main.rs
+
+src\main.rs:
---
+++ 006_project/project/target/debug/project.d
@@ -0,0 +1,1 @@
+C:\Users\Matheus\OneDrive\Desktop\Programação\rust\006_project\project\target\debug\project.exe: C:\Users\Matheus\OneDrive\Desktop\Programação\rust\006_project\project\src\main.rs
