742c89e6ac39c415939709b5e575878a4d24752e
MatheusMendesL <matheusmendelopes@gmail.com>
2026-08-27 13:24:37 -0300

    match

---
+++ 013_match/mathces/target/debug/deps/mathces-92d2c2d3c652c7e4.d
@@ -0,0 +1,5 @@
+C:\Users\Matheus\OneDrive\Desktop\Programação\rust\013_match\mathces\target\debug\deps\mathces-92d2c2d3c652c7e4.d: src\main.rs
+
+C:\Users\Matheus\OneDrive\Desktop\Programação\rust\013_match\mathces\target\debug\deps\libmathces-92d2c2d3c652c7e4.rmeta: src\main.rs
+
+src\main.rs:
---
+++ 013_match/mathces/target/debug/deps/mathces-eed2f88f111e7d13.d
@@ -0,0 +1,5 @@
+C:\Users\Matheus\OneDrive\Desktop\Programação\rust\013_match\mathces\target\debug\deps\mathces-eed2f88f111e7d13.d: src\main.rs
+
+C:\Users\Matheus\OneDrive\Desktop\Programação\rust\013_match\mathces\target\debug\deps\libmathces-eed2f88f111e7d13.rmeta: src\main.rs
+
+src\main.rs:
