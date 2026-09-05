34a2da4532fee23602ccee48367a3e94fb33bc43
MatheusMendesL <matheusmendelopes@gmail.com>
2026-09-05 18:41:38 -0300

    impl verb

--- 028_struct_methods/structs/src/main.rs
+++ 028_struct_methods/structs/src/main.rs
@@ -24,6 +24,11 @@ impl Calculate {
 
         self.a / self.b
     }
+
+    // outra instancia do obj (classe) isso aq tudo é poo soq sem a palavra classe
+    fn b_is_bigger_than(&self, other: &Self) -> bool {
+        self.b > other.b
+    }
 }
 
 fn main() {
@@ -31,9 +36,17 @@ fn main() {
     let methods = [numbers.add(),numbers.sub(), numbers.mul(), numbers.div()];
     let methods_string = ["add", "sub", "mul", "div"];
 
-    for i in 0..=3 {
+    for i in 0..methods.len() {
         println!("Metodo executado foi: {} e o resultado é: {}", methods_string[i], methods[i]);
 
+    }
+
+    let numbers2 = Calculate { a: 10, b: 15 };
+
+    if numbers.b_is_bigger_than(&numbers2) {
+        println!("{} is greater than {}", numbers.b, numbers2.b);
+    } else {
+        println!("{} is greater than {}", numbers2.b, numbers.b);
     }
 
 }
