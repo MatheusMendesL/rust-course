e3e6daec2f18f1728cf025c59f3d26bb4d5efccf
Matheus Mendes Lopes <152028004+MatheusMendesL@users.noreply.github.com>
2026-09-08 17:16:34 -0300

    up

---
+++ debug/deps/enums.d
@@ -0,0 +1,5 @@
+C:\Users\aluno.IPIRANGA\Desktop\insano\rust-course\034_struct_enums\enums\target\debug\deps\enums.d: src\main.rs
+
+C:\Users\aluno.IPIRANGA\Desktop\insano\rust-course\034_struct_enums\enums\target\debug\deps\enums.exe: src\main.rs
+
+src\main.rs:
---
+++ debug/enums.d
@@ -0,0 +1,1 @@
+C:\Users\aluno.IPIRANGA\Desktop\insano\rust-course\034_struct_enums\enums\target\debug\enums.exe: C:\Users\aluno.IPIRANGA\Desktop\insano\rust-course\034_struct_enums\enums\src\main.rs
