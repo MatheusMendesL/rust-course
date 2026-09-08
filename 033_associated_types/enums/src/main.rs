c5b7fb20816c1f203d4dd7ce01bac5125fcd9db2
MatheusMendesL <matheusmendelopes@gmail.com>
2026-09-08 13:46:45 -0300

    enums

--- 033_associated_types/enums/src/main.rs
+++ 033_associated_types/enums/src/main.rs
@@ -2,12 +2,11 @@
 enum PaymentMethodType {
     Credit(String),
     Debit(String),
-    Pix(String),
+    Pix(String, String),
 }
 
 fn main() {
     let visa = PaymentMethodType::Credit(String::from("0034-5678-9012-34567"));
-    let mastercard = PaymentMethodType::Debit(String::from("0034-5678-9012-34597"))
+    let mastercard = PaymentMethodType::Debit(String::from("0034-5678-9012-34597"));
+    let pix = PaymentMethodType::Pix(String::from("matheus@gmail.com"), String::from("password"));
 }
-
-
