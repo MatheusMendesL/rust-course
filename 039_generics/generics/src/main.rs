71f1409384554eaca6cc1d2e811db4dad4a01569
MatheusMendesL <matheusmendelopes@gmail.com>
2026-09-08 23:24:11 -0300

    generics

--- 039_generics/generics/src/main.rs
+++ 039_generics/generics/src/main.rs
@@ -1,3 +1,14 @@
+#[derive(Debug)]
+struct sandwich {}
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", identity::<i32>(5));
+    println!("{}", identity::<f64>(3.14));
+    println!("{}", identity::<&str>("hello"));
+    println!("{:?}", identity::<sandwich>(sandwich {}));
+}
+
+fn identity<T>(value: T) -> T {
+    value
 }
---
+++ 039_generics/generics/target/debug/deps/generics-4aff6e4a35bbe589.d
@@ -0,0 +1,5 @@
+C:\Users\Matheus\OneDrive\Desktop\Programação\rust\039_generics\generics\target\debug\deps\generics-4aff6e4a35bbe589.d: src\main.rs
+
+C:\Users\Matheus\OneDrive\Desktop\Programação\rust\039_generics\generics\target\debug\deps\libgenerics-4aff6e4a35bbe589.rmeta: src\main.rs
+
+src\main.rs:
