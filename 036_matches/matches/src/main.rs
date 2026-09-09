1c7b0d5b0c8b150a2325fafc433e01ab5d8ed5ad
MatheusMendesL <matheusmendelopes@gmail.com>
2026-09-08 22:41:39 -0300

    enums

--- 036_matches/matches/src/main.rs
+++ 036_matches/matches/src/main.rs
@@ -1,5 +1,6 @@
 type Temperature = u32;
 
+#[derive(Debug)]
 enum LaundryCicle {
     Cold(Temperature),
     Hot(Temperature),
@@ -9,14 +10,25 @@ enum LaundryCicle {
 impl LaundryCicle {
     fn wash_laundry(&self) {
         match self {
-            LaundryCicle::Cold(t) => {
-                println!("Running the laundry with cold temperature, that is: {t}");
+            LaundryCicle::Hot(t) => {
+                println!("Running the laundry with hot temperature, that is: {t}");
             }
+            /*
+            aqui ele pefa um por 1 normal
             LaundryCicle::Hot(t) => {
                 println!("Running the laundry with hot temperature, that is: {t}");
             }
             LaundryCicle::Delicate(fabric_type) => {
                 println!("Running the laundry with a delicate cycle for {fabric_type}");
+            }*/
+
+            LaundryCicle::Cold(10) => {
+                println!("Wow the temperature is 10");
+            }
+
+            // o q sobrar vem pra ca
+            other_weather => {
+                println!("Running the laundry with cold temperature, that is: {other_weather:?}");
             }
         }
     }
@@ -24,6 +36,6 @@ impl LaundryCicle {
 
 fn main() {
 
-    let laundry = LaundryCicle::Hot(32);
+    let laundry = LaundryCicle::Cold(10);
     laundry.wash_laundry();
 }
